a162e587243fb6da57a79a4dc34251cdfe258fe4
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-14 09:37:41 +0530

    Implement type checking for tuples and enums in TypeScript

--- Lecture_01/app.js
+++ Lecture_01/app.js
@@ -3,14 +3,43 @@
 function add(a, b) {
     return a + b;
 }
-console.log(add(237, 2));
+// console.log(add(237, 2));
 // type checking for objects
 var person = {
     name: "MN Raza",
     age: 30,
     isMarried: true,
+    skills: [
+        'ReactJS',
+        'React Native'
+    ]
 };
-console.log(person.name);
+// console.log(person.name);
+// console.log(person.skills);
 // type checking for arrays
 var numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
+// console.log(numbers);
+// checking tuple Types
+// order is important in it
+var tuple = [1, "2", true];
+// console.log(tuple);
+// Enums
+var Status;
+(function (Status) {
+    Status[Status["Pending"] = 0] = "Pending";
+    Status[Status["InProgress"] = 1] = "InProgress";
+    Status[Status["Completed"] = 2] = "Completed";
+})(Status || (Status = {}));
+var TaskStatus = Status.InProgress;
+if (TaskStatus === Status.Pending) {
+    console.log("Status is Pending");
+}
+else if (TaskStatus === Status.InProgress) {
+    console.log("Status is In Progress");
+}
+else if (TaskStatus === Status.Completed) {
+    console.log("Status is Completed");
+}
+else {
+    console.log("Invalid Status");
+}
--- Lecture_01/app.ts
+++ Lecture_01/app.ts
@@ -6,27 +6,65 @@ function add(a: number, b: number): number {
     return a + b;
 }
 
-console.log(add(237, 2));
+// console.log(add(237, 2));
 
 
 // type checking for objects
 
-const person :{
+const person: {
     name: string,
     age: number,
-    isMarried: boolean
-} ={
+    isMarried: boolean,
+    skills: string[]
+} = {
     name: "MN Raza",
     age: 30,
     isMarried: true,
+    skills: [
+        'ReactJS',
+        'React Native'
+    ]
 
 }
 
-console.log(person.name);
+// console.log(person.name);
+// console.log(person.skills);
 
 
 // type checking for arrays
 
 const numbers: number[] = [1, 2, 3, 4, 5];
 
-console.log(numbers);
+// console.log(numbers);
+
+// checking tuple Types
+// order is important in it
+const tuple: [number, string, boolean] = [1, "2", true];
+// console.log(tuple);
+
+// Enums
+enum Status {
+    Pending,
+    InProgress,
+    Completed
+}
+
+const TaskStatus = Status.Completed;
+
+if (TaskStatus === Status.Pending) {
+    console.log("Status is Pending");
+}
+else if (TaskStatus === Status.InProgress) {
+    console.log("Status is In Progress");
+}
+else if (TaskStatus === Status.Completed) {
+    console.log("Status is Completed");
+}
+else {
+    console.log("Invalid Status");
+
+}
+
+
+// 
+
