8789bd80d677d311b50b129365b37d7508721fc3
Md Noorullah Raza <mnraza94@gmail.com>
2025-01-14 09:17:30 +0530

    type checking for functions, objects and arrays

--- Lecture_01/app.js
+++ Lecture_01/app.js
@@ -1,4 +1,16 @@
+// TypeScript is a superset of JavaScript
+// type checking for functions and variables
 function add(a, b) {
     return a + b;
 }
-console.log(add(1, 2));
+console.log(add(237, 2));
+// type checking for objects
+var person = {
+    name: "MN Raza",
+    age: 30,
+    isMarried: true,
+};
+console.log(person.name);
+// type checking for arrays
+var numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
--- Lecture_01/app.ts
+++ Lecture_01/app.ts
@@ -1,5 +1,32 @@
-function add(a, b) {
+// TypeScript is a superset of JavaScript
+
+// type checking for functions and variables
+
+function add(a: number, b: number): number {
     return a + b;
 }
 
-console.log(add(1, 2));
+console.log(add(237, 2));
+
+
+// type checking for objects
+
+const person :{
+    name: string,
+    age: number,
+    isMarried: boolean
+} ={
+    name: "MN Raza",
+    age: 30,
+    isMarried: true,
+
+}
+
+console.log(person.name);
+
+
+// type checking for arrays
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+console.log(numbers);
