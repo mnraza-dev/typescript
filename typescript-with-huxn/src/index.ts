29db50de1ecd324ae90fffe6be9ca7b72d54c022
Md Noorullah Raza <mnraza94@gmail.com>
2025-04-30 15:36:38 +0530

    add union type examples

--- typescript-with-huxn/src/index.ts
+++ typescript-with-huxn/src/index.ts
@@ -2,21 +2,21 @@
  * Interscetions Example 01
  */
 type Admin = {
-  name: string;
-  privileges: string[];
+    name: string;
+    privileges: string[];
 };
 
 type Employee = {
-  name: string;
-  startDate: Date;
+    name: string;
+    startDate: Date;
 };
 
 type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
-  name: 'MN Raza',
-  privileges: ['create-server'],
-  startDate: new Date(),
+    name: 'MN Raza',
+    privileges: ['create-server'],
+    startDate: new Date(),
 };
 
 console.log(e1);
@@ -47,5 +47,38 @@ const p1: User = {
 console.log(p1);
 
 /**
- * Intersection Example 03
+ * Union Definition:
+ * 
+ * Unions are used to dclare a type that can have one of several possible types.
+ * Unions are useful when we want to allow a variable or parameter to accept 
+ * multiple types, or when we want a function to return a value that could have more than one type.
+ * 
  */
+type UnionType = string | number | boolean;
+const unionType: UnionType = 'Hello';
+console.log(unionType);
+
+/**
+ * Union Example 01
+ */
+let password: string | number;
+password = '123456';
+console.log(password);
+password = 123456;
+console.log(password);
+
+
+
+
+
+
+
+/**
+ * Literal Types:
+ * 
+ * Literal types are used to declare a type that can only be one of a few specific values.
+ * Literal types are useful when we want to ensure that a variable or parameter has a specific value.
+ */
+type LiteralType = 'hello' | 'world' | 'goodbye';
+const literalType: LiteralType = 'hello';
+console.log(literalType);
